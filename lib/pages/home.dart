3431d5851ad3675a88fc612215f8c98519a1723a
mudlobster <shatukho.ff@gmail.com>
2022-05-13 09:08:31 +0300

    update 2

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foxygram/pages/util/bubble_stories.dart';
 
 class UserHome extends StatelessWidget {
   const UserHome({Key? key}) : super(key: key);
@@ -6,11 +7,47 @@ class UserHome extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home',
-        style: TextStyle(fontSize: 50),
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Text('Foxygram', style: TextStyle(color: Colors.black)),
+          Row(
+            children: [
+          Icon(Icons.add),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+          child: Icon(Icons.favorite),
+          ),
+          Icon(Icons.share),
+        ],
+          )
+      ],
+        )),
+      body: Column(
+          children: [
+            //stories
+        Container(
+          height: 130,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              BubbleStories(text: 'Nadya'),
+              BubbleStories(text: 'Sasha'),
+              BubbleStories(text: 'Xenia'),
+              BubbleStories(text: 'Dasha'),
+              BubbleStories(text: 'Alina'),
+              BubbleStories(text: 'Sveta'),
+              BubbleStories(text: 'Anya'),
+              BubbleStories(text: 'Ksusha'),
+            ],
       ),
-    );
+        )
+    //posts
+    ],
+    ));
   }
 }
